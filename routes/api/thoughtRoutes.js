2c717a1d50b0d9b1dd4a01055ad0ff165f2bbba2
CucciPro <josej@email.arizona.edu>
2020-11-08 04:24:23 -0700

    frogot to setup thought routes. All set now

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} = require('../../controllers/thoughtController');
+
+// api/thoughts
+router.route("/").get(getThoughts).post(createThought);
+
+// api/thoughts/:id
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+// api/thoughts/:id/reactions
+router.route('/:id/reactions').post(addReaction)
+
+// api/thoughts/:id/reactions/:reactionId
+router.route('/:id/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
